f0f75b4d6199e7a8ef753b08b3e76ca9e7a61221
Simphiwe Dangazeala <Simphiwe@explore-datascience.net>
2019-03-19 13:49:44 +0200

    inserting name and email

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mypackage',
+    name='mypackages',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<username>/<package-name>',
-    author='<Your Name>',
-    author_email='<Your Email>'
+    url='https://github.com/simied/mypackages',
+    author='simied',
+    author_email='nondaba.simi@gmail.com'
 )
